de4cde5602d47b80cabd88c6290b24be45fd5678
tssibanda <55544998+tssibanda@users.noreply.github.com>
2019-09-19 14:41:58 +0100

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+###########################################################
+# Program Description: Program to display "Hello World!"
+# Date: 19 September 2019
+# Author: Thamsanqa Steven Sibanda
+###########################################################
+print("Hello World!")
