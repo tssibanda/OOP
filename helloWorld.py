56742cffa3fb0a8937d917f9fc9fc79ef64f36ea
tssibanda <55544998+tssibanda@users.noreply.github.com>
2019-09-19 15:04:51 +0100

    Add files via upload

--- helloWorld.py
+++ helloWorld.py
@@ -4,3 +4,10 @@
 # Author: Thamsanqa Steven Sibanda
 ###########################################################
 print("Hello World!")
+y_name = input("What is your name")
+y_age = input("How old are you")
+limt = 18
+if int(y_age) > 18:
+    print("Welcome ",y_name)
+else:
+    print("This AI thinks you are too young to interact with it")
